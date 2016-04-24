65ad4dd1e20e3ec96f9f8f1d28eec42d8c22cc50
ayman20043 <ayman20043@yahoo.com>
2016-04-24 17:08:22 +0200

    Login and register View changes and begin of contact us

--- WebApplication/Controllers/AdminController.cs
+++ WebApplication/Controllers/AdminController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
@@ -11,6 +13,57 @@ namespace WebApplication.Controllers
         // GET: Admin
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            try
+            {
+                using (TouchContext db = new TouchContext())
+                {
+                    if (db.ContactUs.Any())
+                    {
+                        var existingRow = db.ContactUs.FirstOrDefault();
+                        return View(existingRow);
+                    }
+                    else
+                    {
+                        return View(new ContactUs());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //Todo Write Some Exception Logging Technique 
+                // throw;
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ContactUs(ContactUs input)
+        {
+            try
+            {
+                using (TouchContext db = new TouchContext())
+                {
+                    if (input.Id > 0)
+                    {
+                        db.Entry(input).State = EntityState.Modified;
+                    }
+                    else
+                    {
+                        db.ContactUs.Add(input);
+                    }
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                // throw;
+            }
             return View();
         }
     }
