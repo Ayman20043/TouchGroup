fce662f6c9e2484bcabc4cfc9cc279a3d23edce4
ayman20043 <ayman20043@yahoo.com>
2016-05-13 18:16:21 +0200

    General Improvements

--- WebApplication/Controllers/AdminController.cs
+++ WebApplication/Controllers/AdminController.cs
@@ -112,7 +112,7 @@ namespace WebApplication.Controllers
             }
 
         }
-
+        [HttpPost]
         public ActionResult CreatMember(TeamMemberViewModel Input)
         {
             try
