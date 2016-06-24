b781fe0228cb754110cb9b211d904b7b548732de
ayman20043 <ayman20043@yahoo.com>
2016-06-24 16:50:26 +0200

    Projects Finished

--- WebApplication/Controllers/PortfolioController.cs
+++ WebApplication/Controllers/PortfolioController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication.Models;
+using WebApplication.Models.ViewModel;
 
 namespace WebApplication.Controllers
 {
@@ -11,7 +13,37 @@ namespace WebApplication.Controllers
         // GET: Portfolio
         public ActionResult Index()
         {
-            return View();
+            var result = new PortfolioViewModel();
+            using (TouchContext db=new TouchContext())
+            {
+                var projects= db.Projects.Include("Category").Include("ProjectImages").ToList();
+                result.Projects=new List<HomeProjectViewModel>();
+                foreach (var project in projects)
+                {
+                    var temp = new HomeProjectViewModel();
+                    temp.Id = project.Id;
+                    temp.Name=project.Name;
+                    temp.Category = project.Category.Name;
+                    if (project.SubCategoryId != null && project.SubCategoryId > 0)
+                        temp.SubCategory = db.SubCategories.SingleOrDefault(e => e.Id == project.SubCategoryId.Value).Name;
+                    if(project.ProjectImages.Any())
+                    temp.FileName = project.ProjectImages.FirstOrDefault().FileName;
+                    result.Projects.Add(temp);
+                }
+                result.Categories = db.Categories.Include("Subcategories").ToList();
+                result.SubCategories = db.SubCategories.ToList();
+            }
+            return View(result);
+        }
+
+        public ActionResult Project(int id)
+        {
+            using (TouchContext db=new TouchContext())
+            {
+                var result = db.Projects.Include("ProjectImages").SingleOrDefault(e => e.Id == id);
+                return View(result); 
+            }
+           
         }
     }
 }
---
+++ WebApplication/Models/ViewModel/PortfolioViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Models.ViewModel;
+
+namespace WebApplication.Models
+{
+    public class PortfolioViewModel
+    {
+        public List<HomeProjectViewModel> Projects { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<SubCategory> SubCategories { get; set; }
+
+    }
+}
