1f3386bc83a3f45b26444d7cb1e19ba27d7dbcba
ayman20043 <ayman20043@yahoo.com>
2016-06-25 01:46:56 +0200

    Team members Added

---
+++ WebApplication/Controllers/OurTeamController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class OurTeamController : Controller
+    {
+        // GET: OurTeam
+        public ActionResult Index()
+        {
+            using (TouchContext db=new TouchContext())
+            {
+                return View(db.TeamMembers.ToList());
+            }
+          
+        }
+    }
+}
---
+++ WebApplication/Models/ViewModel/HomeProjectViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models.ViewModel
+{
+    public class HomeProjectViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string SubCategory { get; set; }
+        public string FileName { get; set; }
+
+    }
+}
