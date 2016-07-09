c18d2f19dafa55575c7d92c19abb4156d46341fe
ayman20043 <ayman20043@yahoo.com>
2016-07-09 07:45:05 +0300

    Migrations Fixed & General Fixes

---
+++ WebApplication/Migrations/201607090329340_reCreateDB.cs
@@ -0,0 +1,229 @@
+namespace WebApplication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class reCreateDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BackgroundImages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TitleColor = c.String(),
+                        Extention = c.String(),
+                        PicturePath = c.String(),
+                        IsActive = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.CareerInfoes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Content = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Projects",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Location = c.String(),
+                        Area = c.Int(nullable: false),
+                        Client = c.String(),
+                        LogoPath = c.String(),
+                        CategoryId = c.Int(nullable: false),
+                        SubCategoryId = c.Int(),
+                        IsActive = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .ForeignKey("dbo.SubCategories", t => t.SubCategoryId)
+                .Index(t => t.CategoryId)
+                .Index(t => t.SubCategoryId);
+            
+            CreateTable(
+                "dbo.ProjectImages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FileName = c.String(),
+                        ProjectId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Projects", t => t.ProjectId, cascadeDelete: true)
+                .Index(t => t.ProjectId);
+            
+            CreateTable(
+                "dbo.SubCategories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        CategoryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.CompanyProfiles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        SectionName = c.String(),
+                        Content = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ContactUs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        AddressEgy = c.String(),
+                        TelephoneEgy = c.String(),
+                        MailEGY = c.String(),
+                        FaxEgy = c.String(),
+                        MobileEgy = c.String(),
+                        AddressUAE = c.String(),
+                        TelephoneUAE = c.String(),
+                        MailUAE = c.String(),
+                        FaxUAE = c.String(),
+                        MobileUAE = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ContactUsMessages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        From = c.String(),
+                        Subject = c.String(),
+                        Message = c.String(),
+                        IsRead = c.Boolean(nullable: false),
+                        SendDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.HomeImages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        Extention = c.String(),
+                        PicturePath = c.String(),
+                        IsActive = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.JobApplications",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FullName = c.String(),
+                        Position = c.String(),
+                        Phone = c.String(),
+                        Email = c.String(),
+                        CvPath = c.String(),
+                        IsViewed = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Licenses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Content = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Services",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Content = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.SocialLinks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Facebook = c.String(),
+                        pinterest = c.String(),
+                        Instgrame = c.String(),
+                        LinkedIn = c.String(),
+                        GooglePlus = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.TeamMembers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Position = c.String(),
+                        Extention = c.String(),
+                        PicturePath = c.String(),
+                        Details = c.String(),
+                        CvPath = c.String(),
+                        Phone = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Projects", "SubCategoryId", "dbo.SubCategories");
+            DropForeignKey("dbo.SubCategories", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.ProjectImages", "ProjectId", "dbo.Projects");
+            DropForeignKey("dbo.Projects", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.SubCategories", new[] { "CategoryId" });
+            DropIndex("dbo.ProjectImages", new[] { "ProjectId" });
+            DropIndex("dbo.Projects", new[] { "SubCategoryId" });
+            DropIndex("dbo.Projects", new[] { "CategoryId" });
+            DropTable("dbo.TeamMembers");
+            DropTable("dbo.SocialLinks");
+            DropTable("dbo.Services");
+            DropTable("dbo.Licenses");
+            DropTable("dbo.JobApplications");
+            DropTable("dbo.HomeImages");
+            DropTable("dbo.ContactUsMessages");
+            DropTable("dbo.ContactUs");
+            DropTable("dbo.CompanyProfiles");
+            DropTable("dbo.SubCategories");
+            DropTable("dbo.ProjectImages");
+            DropTable("dbo.Projects");
+            DropTable("dbo.Categories");
+            DropTable("dbo.CareerInfoes");
+            DropTable("dbo.BackgroundImages");
+        }
+    }
+}
