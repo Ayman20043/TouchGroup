893a8cd0545d6c074c6320bd907b0095612010ec
ayman20043 <ayman20043@yahoo.com>
2016-07-09 04:20:49 +0300

    General Fixes And Updates

---
+++ WebApplication/Models/BackgroundImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class BackgroundImage
+    {
+        public int Id { get; set; }
+        [Display(Name = "Title Color")]
+        public string TitleColor { get; set; }
+        public string Extention { get; set; }
+        [Display(Name = "Picture")]
+
+        public string PicturePath { get; set; }
+        public Boolean IsActive { get; set; }
+    }
+}
