5bf8dae0fdaf6361472ce814c2a1910275945705
ayman20043 <ayman20043@yahoo.com>
2016-09-03 11:32:23 +0200

    PreCaolifications Added

--- WebApplication/Controllers/AboutUsController.cs
+++ WebApplication/Controllers/AboutUsController.cs
@@ -17,5 +17,12 @@ namespace WebApplication.Controllers
             }
 
         }
+
+        public ActionResult Download()
+        {
+            byte[] fileBytes = System.IO.File.ReadAllBytes(Server.MapPath("~/File/Portfolio/TCG-PRE COALIFICATION-v9-small.pdf"));
+            string fileName = "TCG-PRE COALIFICATION.pdf";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
     }
 }
